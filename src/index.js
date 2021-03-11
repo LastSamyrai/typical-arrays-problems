12c7956835193f07445df04a548cb47ebee658e6
Ivan <last1samyrai@gmail.com>
2021-03-11 22:13:39 +0200

    fix: fix bugs in function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 
 exports.min = function min (array) {
-    if (array.length == 0) {
-        return 0;
+    if (array.length > 0) {
+        return Math.min.apply(null, array);
     }
     else {
-        return Math.min.apply(null, array);
+        return 0;
     }
 }
 
 exports.max = function max (array) {
-    if (array.length == 0) {
-        return 0;
+    if (array.length > 0) {
+        return Math.max.apply(null, array);
     }
     else {
-        return Math.max.apply(null, array);
+        return 0;
+
     }
 }
 
 exports.avg = function avg (array) {
-    if (array.length == 0) {
-        return 0;
+    if (array.length > 0) {
+        return array.reduce((a, b) => (a + b)) / array.length;
     }
     else {
-        return array.reduce((a, b) => (a + b)) / array.length;
+        return 0;
+
     }
 }
