771716bbdd2daeffb8ccbaa24a0d7dc5a48647bc
Ivan <last1samyrai@gmail.com>
2021-03-11 22:16:24 +0200

    fix: fix bugs in function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 exports.min = function min (array) {
-    if (array.length > 0) {
+    if (array.length > 0 && Array.isArray(array)) {
         return Math.min.apply(null, array);
     }
     else {
@@ -9,21 +9,19 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-    if (array.length > 0) {
+    if (array.length > 0 && Array.isArray(array)) {
         return Math.max.apply(null, array);
     }
     else {
         return 0;
-
     }
 }
 
 exports.avg = function avg (array) {
-    if (array.length > 0) {
+    if (array.length > 0 && Array.isArray(array)) {
         return array.reduce((a, b) => (a + b)) / array.length;
     }
     else {
         return 0;
-
     }
 }
